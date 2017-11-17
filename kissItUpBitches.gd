fff980907857debd5e09d481c4bf3bc813f13cf3
typedefJorge <jnone001@fiu.edu>
2017-11-16 22:37:51 -0500

    animating kiss emoji around the screen

--- kissItUpBitches.gd
+++ kissItUpBitches.gd
@@ -1,10 +1,35 @@
-extends Sprite
+extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+export var startingAnimFrame = 0
+
+onready var sprite = get_child(0)
+onready var tween = get_child(1)
+
+var currentFrame = startingAnimFrame
+
+var step = 100
+
+var positionOffsets = [
+	Vector2(-step, -step),
+	Vector2(-step, -step),
+	Vector2(0, -step),
+	Vector2(step, -step),
+	Vector2(step, step/2),
+	Vector2(step, -step/2),
+	Vector2(step, step),
+	Vector2(0, step),
+	Vector2(-step, step),
+	Vector2(-step, step)
+]
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
+
+func _flip_and_move():
+	sprite.set_flip_h(!sprite.is_flipped_h())
+	sprite.set_pos(sprite.get_pos() + positionOffsets[currentFrame])
+	
+	if (currentFrame < (positionOffsets.size() - 1)):
+		currentFrame = currentFrame + 1
+	else:
+		currentFrame = 0
